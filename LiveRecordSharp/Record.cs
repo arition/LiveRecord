5e2a425c5d9435b9b54ee853fcf04e2107a615b0
arition <aritionkb@gmail.com>
2016-04-25 19:27:30 -0700

    clean up

--- LiveRecordSharp/Record.cs
+++ LiveRecordSharp/Record.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using log4net;
-using log4net.Repository.Hierarchy;
 using LiveRecordSharp.LiveSites;
 
 namespace LiveRecordSharp
